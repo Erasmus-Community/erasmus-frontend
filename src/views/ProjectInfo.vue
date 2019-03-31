a4392da80cf21e815c7e01f1da661d89e464b23f
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2019-03-31 18:26:07 +0100

    adding projectinfo information

---
+++ src/views/ProjectInfo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Info Project
+    </div>
+</template>
+
+<script>
+    export default {  
+        props: ['proj'],
+        name: ProjectInfo,
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-
+        <h1>All Projects</h1>
+        <ProjectInfo v-for="proj in projects" :key="proj.key" :proj="proj"></ProjectInfo>
     </div>
 </template>
 
 <script>
+    import ProjectInfo from '@/components/ProjectInfo.vue'
+    
     export default {
-        
+        name: Project,
+        components: {
+            ProjectInfo
+        }
     }
 </script>
 
