f3ec5a3c2cd87e4d823c3ce1f7824e4ea4dc1443
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2019-06-25 01:42:18 +0100

    working on create orgs section

--- src/views/CreateOrg.vue
+++ src/views/CreateOrg.vue
@@ -2,35 +2,47 @@
     <v-content class="ma-3">
         <v-form v-model="valid">
             <v-text-field
-                v-model="firstname"
-                :rules="nameRules"
-                :counter="10"
-                label="Organisation Name"
-                required
+              v-model="orgname"
+              :error-messages="nameErrors"
+              label="Organisation Name"
+              required
+              @input="$v.orgname.$touch()"
+              @blur="$v.orgname.$touch()"
+            ></v-text-field>
+            <v-text-field
+              v-model="description"
+              label="Description"
+              @input="$v.description.$touch()"
+              @blur="$v.description.$touch()"
             ></v-text-field>
             <v-text-field
                 v-model="email"
-                :rules="emailRules"
+                :error-messages="emailErrors"
                 label="E-mail"
                 required
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
             ></v-text-field>
             <v-select
                 v-model="country"
                 :items="countries"
-                :error-messages="selectErrors"
+                item-text="name"
+                :error-messages="countriesErrors"
                 label="Country"
                 required
-                @change="$v.select.$touch()"
-                @blur="$v.select.$touch()"
+                @change="$v.country.$touch()"
+                @blur="$v.country.$touch()"
+            ></v-select>
+            <v-select
+              v-model="erasmus"
+              :items="erasmus"
+              @change="$v.erasmus.$touch()"
+              @blur="$v.erasmus.$touch()"
             ></v-select>
             <v-select
                 v-model="interests"
                 :items="items"
-                :error-messages="selectErrors"
                 label="Interests"
-                required
-                @change="$v.select.$touch()"
-                @blur="$v.select.$touch()"
             ></v-select>
             <v-btn @click="submit">submit</v-btn>
             <v-btn @click="clear">clear</v-btn>
@@ -40,32 +52,59 @@
 
 <script>
   import axios from 'axios'
+  import { validationMixin } from 'vuelidate'
+  import { required, email } from 'vuelidate/lib/validators'
+  
   export default {
+    mixins: [validationMixin],
+    
+    validations : {
+      orgname: { required },
+      email: { required, email},
+      country: { required }
+
+    },
+
     data: () => ({
       valid: false,
-      firstname: '',
-      lastname: '',
-      country: null,
+      orgname: '',
+      email: '',
+      country: '',
+      description: '',
       countries: [],
       interests: null,
+      erasmus: null,
       items: [], // to be changed
-
-      // Rules for input
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters'
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid'
-      ]
     }),
 
     created(){
       axios.get('https://restcountries.eu/rest/v2/all').then( info => {
-        this.countries = info; 
+        this.countries = info.data; 
       });
+    },
+
+    computed: {
+      nameErrors () {
+        const errors = []
+        if (!this.$v.orgname.$dirty) return errors
+        !this.$v.orgname.required && errors.push('Organisation Name is required.')
+        return errors
+      },
+
+      countriesErrors () {
+        const errors = []
+        if (!this.$v.country.$dirty) return errors
+        !this.$v.country.required && errors.push('Item is required')
+        return errors
+      },
+
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      }
     },
 
     methods:{
@@ -73,9 +112,16 @@
         console.log(event);
       },
       
-      submit: function(event){
-        console.log(event);
+    submit() {
+      console.log('submit!')
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        console.log('error')
+      } else {
+        // do your submit logic here
+        console.log('ok');
       }
+    }
     }
   }
 </script>
