cc47ac7ee5e381d75d36dad23f86359cf865890e
Paulo Cardoso <pcardoso.lei@gmail.com>
2020-07-11 16:06:57 +0100

    moving to vuetify 2.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
-    <div></div>
+  <hello-world />
 </template>
 
 <script>
+import HelloWorld from '../components/HelloWorld'
+
 export default {
+  name: 'Home',
+
+  components: {
+    HelloWorld
+  }
 }
 </script>
