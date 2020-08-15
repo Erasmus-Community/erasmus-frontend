631d917fe01dc67a3f92b754f30e9a798613f4b3
Paulo Cardoso <paulo.cardoso@jumpseller.com>
2020-08-16 00:26:30 +0100

    adding css changes

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,7 @@
 <template>
   <v-form v-model="valid" class="mx-auto">
     <v-text-field v-model="name" label="Username" required></v-text-field>
-    <v-password-field v-model="password" label="Password" required></v-password-field>
+    <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" label="Password" :rules="[rules.required, rules.min]"></v-text-field>
     <v-btn color="success" class="mr-4" @click="login">Sign Up</v-btn>
     <a href="/login" class="mr-4">Go back to login page</a>
   </v-form>
@@ -10,6 +10,14 @@
 <script>
 export default {
   name: 'signup',
+  data () {
+    return {
+      valid: false,
+      name: '',
+      password: '',
+      showPwd: false
+    }
+  },
   methods: {
     login () {
       this.$router.push('/login')
