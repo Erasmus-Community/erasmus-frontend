2436d1d2bee5ab190b8a863e80f3380cc404f97c
Paulo Cardoso <paulo.cardoso@jumpseller.com>
2020-08-15 17:54:00 +0100

    working on the orgs

--- src/views/Orgs.vue
+++ src/views/Orgs.vue
@@ -1,20 +1,31 @@
-<template>
-    <v-content class="ma-3">
-        <h1 class="text-xs-center"> Organizations </h1>
+<template class="ma-3">
+    <v-main class="ma-3">
+        <h1 class="text-center">Organizations</h1>
+        <!-- This is a search bar. TODO:
         <v-container>
           <v-row>
-            <div v-for="org in orgs" v-bind:key="org.id">
+            <v-col class="d-flex col-6">
+             <country-select class="v-btn" v-model="country" :country="country" topCountry="PT" />
+            </v-col>
+            <v-col class="d-flex col-3">
+              <v-btn :click="search">Search</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+        -->
+        <v-container>
+          <v-row v-for="org in orgs" v-bind:key="org.id">
               <v-col name="country" class="col-3">{{org.country}}</v-col>
               <v-col name="org_name" class="col-6">{{org.name}}</v-col>
-              <v-col name="href" class="col-3">
+              <v-col name="org_email" class="col-3">{{org.email}}</v-col>
+            <!--  <v-col name="href" class="col-3"> # this is to do after I have more time to do like a overview of the orgs
                 <a :href="'/orgInfo/' + org.id">See More</a>
-              </v-col>
-              </div>
+              </v-col> -->
           </v-row>
           <div >
           </div>
         </v-container>
-    </v-content>
+    </v-main>
 </template>
 
 <script>
@@ -27,6 +38,7 @@ export default {
   },
   data: function () {
     return {
+      country: '',
       orgs: [
         {
           'id': '5ee3b3e9144d50ad015f6117',
@@ -128,13 +140,23 @@ export default {
   },
 
   methods: {
-    fetchOrgs () {
-      axios.get('http://localhost:8000/api/orgs').then(response => {
+    fetchOrgs (options = null) {
+      let url = 'http://localhost:8000/api/orgs'
+
+      if (options) {
+        debugger
+      }
+
+      axios.get(url).then(response => {
         this.orgs = response.data
         console.log(this.orgs)
       }).catch(error => {
         console.log(error)
       })
+    },
+
+    search () {
+      this.fetchOrgs(this.country)
     }
   }
 }
