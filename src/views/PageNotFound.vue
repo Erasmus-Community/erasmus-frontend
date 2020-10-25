50f2254f76b02cc11019801160f78c66751990da
Paulo Cardoso <pcardoso.lei@gmail.com>
2020-10-25 22:04:51 +0000

    adding pageNotFound + image

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container fill-height>
+        <v-row justify="center" align="center">
+            <v-col cols="12" sm="4">
+                The Page you were looking for does not exist.
+            </v-col>
+        </v-row>
+        <v-img contain :src="require('@/assets/img/404.png')"></v-img>
+    </v-container>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
